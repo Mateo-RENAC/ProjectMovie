5f1aa6d8651f93ab4bd7adb44f0bad8b85a801f6
Mateo-RENAC <mateo.renac@gmail.com>
2024-08-27 14:50:56 +0200

    Initial Migration

---
+++ Models/Entities/Movie.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjectMovie.Models.Entities
+{
+	public class Movie
+	{
+		public Guid Id { get; set; }
+
+		public required string Title { get; set; }
+
+		public string? Description { get; set; }
+
+		public required string Author { get; set; }
+
+
+	}
+
+
+}
