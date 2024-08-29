7d0af483f2066bba7e5ca1d9cc9ac5d23954cebf
Mateo-RENAC <mateo.renac@gmail.com>
2024-08-29 11:02:00 +0200

    Edit ne fonctionne tjr pas

--- Services/Movie/MovieServices.cs
+++ Services/Movie/MovieServices.cs
@@ -77,13 +77,48 @@ namespace ProjectMovie.Services
             return await _context.Movie.AnyAsync(e => e.Id == id);
         }
 
-        public async Task UpdateMovieAsync(MovieViewModel movie)
-		{
-			_context.Update(movie);
-			await _context.SaveChangesAsync();
-		}
+        public async Task UpdateMovieAsync(MovieViewModel movieViewModel)
+        {
+            // Check if an image file has been uploaded
+            if (movieViewModel.Picture != null && movieViewModel.Picture.Length > 0)
+            {
+                // Define the directory path to save the image using IWebHostEnvironment
+                var uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
 
-		public async Task Delete(Guid id)
+                // Ensure the directory exists
+                if (!Directory.Exists(uploadsFolder))
+                {
+                    Directory.CreateDirectory(uploadsFolder);
+                }
+
+                // Create a unique file name to avoid overwriting existing files
+                var uniqueFileName = Guid.NewGuid().ToString() + "" + Path.GetFileName(movieViewModel.Picture.FileName);
+
+                // Combine the folder path with the unique file name
+                var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                // Save the file to the server
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await movieViewModel.Picture.CopyToAsync(fileStream);
+                }
+
+                // Create a new Movie object to save the data Guido the database
+                var movie = new Movie
+                {
+                    Title = movieViewModel.Title,
+                    Author = movieViewModel.Author,
+                    Description = movieViewModel.Description,
+                    PathPicture = uniqueFileName // Save the ImageUrl in the Movie object
+                };
+
+                // Add the new movie to the context
+                _context.Update(movie);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(Guid id)
 		{
 			var movie = await _context.Movie.FindAsync(id);
 			if (movie != null)
